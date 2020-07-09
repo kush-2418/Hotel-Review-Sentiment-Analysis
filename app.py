57812d7d9f42b174b4ccf0c4625e429dacd5a77a
Kush Shrivastava <37059662+kush-2418@users.noreply.github.com>
2020-07-09 10:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import pickle
 import numpy as np
 import nltk
 nltk.download('stopwords')
+nltk.download('wordnet')
 
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
